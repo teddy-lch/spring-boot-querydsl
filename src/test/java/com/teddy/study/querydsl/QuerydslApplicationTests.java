6fb90f7c81b7fea180686fec769b34500d5a262a
lee ch <cndgml00123@gmail.com>
2023-02-12 18:02:52 +0900

    h2 db 연결 설정

--- src/test/java/com/teddy/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/teddy/study/querydsl/QuerydslApplicationTests.java
@@ -7,12 +7,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.*;
 
+@Commit
 @Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
