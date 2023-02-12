e43c24a50a2ec68f8ea26066e0958d89ff3f38c3
lee ch <cndgml00123@gmail.com>
2023-02-12 17:05:00 +0900

    init commit

---
+++ src/test/java/com/teddy/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teddy.study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
